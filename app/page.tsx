897bdab6d179b942a0006eb23a466c3f2204c1ab
dhirajaher <dhirajaher12345@gmail.com>
2025-09-15 13:32:38 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,8 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center flex-1 px-4">
         {/* Title */}
         <h1 className="text-2xl md:text-3xl lg:text-6xl font-medium mb-8 text-center">
-          What's on your mind today?
+          <h1>What&apos;s on your mind today?</h1>
+
         </h1>
 
         {/* Input field */}
