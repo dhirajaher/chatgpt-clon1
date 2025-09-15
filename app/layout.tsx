6c8bb4f770fa316d61c601628d0ef13efad88757
dhirajaher <dhirajaher12345@gmail.com>
2025-09-15 13:54:43 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-900 text-white min-h-screen`}>
+      <body className={`${inter.className} bg-white text-black min-h-screen`}>
+
         <div className="flex flex-col min-h-screen">
           {/* All page content (Header + Page children) */}
           {children}
